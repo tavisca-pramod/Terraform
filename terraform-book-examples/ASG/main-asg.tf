de64aa5dbfab52fcf4d8e2fcbe2407d968353579
root <root@ip-172-31-2-130.ap-south-1.compute.internal>
2017-08-10 06:53:36 +0000

    Terraform script for ASG

--- terraform-book-examples/ASG/main-asg.tf
+++ terraform-book-examples/ASG/main-asg.tf
@@ -7,7 +7,7 @@ resource "aws_launch_configuration" "example" {
  image_id = "ami-47205e28"
  instance_type = "t2.micro"
  security_group = ["${aws_security_group.instance.id}"]
- 
+ key_name = "rajshah-mumbai"
  user_data = <<-EOF
 		#!/bin/bash
 		yum update -y
@@ -22,3 +22,53 @@ resource "aws_launch_configuration" "example" {
 }
 
 }
+
+variable "server_port"{
+ description = "Enter the port for server HTTP requests"
+}
+
+resource "aws_security_group" "instance" {
+
+ name = "terraform-ASG-instance"
+
+ ingress {
+ from_port = "${var.server_port}"
+ to_port = "${var.server_port}"
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+}
+ ingress {
+ from_port = 22
+ to_port = 22
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+}
+ egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+}
+
+ lifecycle {
+ create_before_destroy = true
+}
+}
+
+ data "aws_availability_zones" "all"{} 
+
+
+ resource "aws_autoscaling_group" "example"{
+ launch_configuration = "${aws_launch_configuration.example.id}"
+ availability_zones = ["${data.aws_avalibility_zones.all.names}"]
+ min_size = 2
+ max_size = 4
+ tag {
+  key = "Name"
+  value = "terraform_ASG_example"
+  propagate_at_launch = true
+}
+
+}
+
+
